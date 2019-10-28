3397c5a9a3da0215f2acd1005fd838baa05e01ab
ymko home <ymko@n3n.co.kr>
2019-10-28 18:49:21 +0900

    change base, to next.js

---
+++ components/header.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+const linkStyle = {
+    marginRight: '1rem'
+}
+
+const Header = () => {
+    return (
+        <div>
+            <Link href="/"><a style={linkStyle}>Home</a></Link>
+            <Link href="/about"><a style={linkStyle}>About</a></Link>
+            <Link prefetch href="/ssr-test"><a style={linkStyle}>SSR Test</a></Link>
+        </div>
+    );
+};
+
+export default Header;
---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Layout from '../components/layout'
+
+const About = () =>(
+    <Layout>
+        <style jsx>
+            {`
+                h2 {
+                    color: green
+                }
+            `}
+        </style>
+        
+        <h2> Hello, I'm ...</h2>
+    </Layout>
+)
+
+export default About;
